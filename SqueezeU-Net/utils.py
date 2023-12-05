be3abcc364acc0b72b85154c854705aeef009fec
GiacomoBellini <bellini.1970896@studenti.uniroma1.it>
2023-12-05 16:29:48 +0100

    squeeze unet val and test

--- SqueezeU-Net/utils.py
+++ SqueezeU-Net/utils.py
@@ -2,6 +2,8 @@ from dataset import RandomGenerator, Synapse_dataset
 #from torchvision import transforms
 from torch.utils.data import DataLoader
 from rich.progress import *
+from rich import print
+from medpy import metric
 
 def get_progress_bar() -> Progress:
 
@@ -29,9 +31,43 @@ def get_dataset_train(batch_size: int, num_workers: int, pin_memory: bool) -> Sy
         #     ]
         # )
     )
-
     return DataLoader(
         dataset=train_dataset, batch_size=batch_size, shuffle=True, 
+        num_workers=num_workers, pin_memory=pin_memory
+    )
+
+def get_dataset_test(batch_size: int, num_workers: int, pin_memory: bool) -> Synapse_dataset:
+    root_path = r"C:\Users\giaco\Documents\GIACOMO\SAPIENZA\AML\Project\project_TransUNet\data\Synapse\test_vol_h5"
+    list_dir = r'C:\Users\giaco\Documents\GIACOMO\SAPIENZA\AML\Project\project_TransUNet\TransUNet\lists\lists_Synapse'
+
+    test_dataset = Synapse_dataset(base_dir=root_path, list_dir=list_dir, split='test_vol',
+        # transform=transforms.Compose(
+        #     [
+        #         RandomGenerator(output_size=[512, 512])
+        #     ]
+        # )
+    )
+
+    return DataLoader(
+        dataset=test_dataset, batch_size=batch_size, shuffle=True, 
+        num_workers=num_workers, pin_memory=pin_memory
+    )
+
+
+def get_dataset_validation(batch_size: int, num_workers: int, pin_memory: bool) -> Synapse_dataset:
+    root_path = r"C:\Users\giaco\Documents\GIACOMO\SAPIENZA\AML\Project\project_TransUNet\data\Synapse\val_vol_h5"
+    list_dir = r'C:\Users\giaco\Documents\GIACOMO\SAPIENZA\AML\Project\project_TransUNet\TransUNet\lists\lists_Synapse'
+
+    val_dataset = Synapse_dataset(base_dir=root_path, list_dir=list_dir, split='val_vol',
+        # transform=transforms.Compose(
+        #     [
+        #         RandomGenerator(output_size=[512, 512])
+        #     ]
+        # )
+    )
+
+    return DataLoader(
+        dataset=val_dataset, batch_size=batch_size, shuffle=True, 
         num_workers=num_workers, pin_memory=pin_memory
     )
 
@@ -51,3 +87,34 @@ def metric_has_improved(metric, metric_best, lt_or_gt: str):
         raise ValueError(f"lt_or_gt should be either \"min\" or \"max\", got {lt_or_gt} instead")
 
     return is_best
+
+def calculate_dice_metric_per_case(pred, gt):
+
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    
+    if pred.sum() > 0 and gt.sum()>0:
+        
+        return metric.binary.dc(pred, gt)
+    
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 1
+    
+    else:
+        return 0
+    
+def calculate_jaccard_metric_per_case(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    
+    if pred.sum() > 0 and gt.sum()>0:
+
+        jaccard = metric.binary.jc(pred, gt)
+        
+        return jaccard
+    
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 1
+    
+    else:
+        return 0
