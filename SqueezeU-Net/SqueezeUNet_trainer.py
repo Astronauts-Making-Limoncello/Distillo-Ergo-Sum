0eb706dc5daede435e229996fcd3c5b3fb40d4e3
Daniele Solombrino <solombrino.1743111@studenti.uniroma1.it>
2023-12-06 07:43:46 +0100

    [WIP] add support for SqueezeU-Net train from scratch

---
+++ SqueezeU-Net/SqueezeUNet_trainer.py
@@ -0,0 +1,264 @@
+from rich import print
+from rich.pretty import pprint
+
+import SqueezeUnet_args as args
+
+import torch
+
+from utilities.Prints import print_device_name
+
+from squeezeunet_torch import SqueezeUNet
+
+from utilities.Prints import print_num_trainable_parameters
+
+from data.dataset_synapse import Synapse_dataset
+
+from torch.utils.data import DataLoader
+
+from utilities.Prints import print_data_summary
+
+import wandb
+
+from utilities.Model import get_num_trainable_parameters
+
+from utilities.Progress import get_progress_bar
+
+from rich.progress import Progress
+
+from wandb.wandb_run import Run
+
+from torch.nn.modules.loss import CrossEntropyLoss
+from utilities.DiceLoss import DiceLoss
+
+import os
+
+### --- model --- ###
+
+def _init_model(args: args, device) -> SqueezeUNet:
+    
+    model = SqueezeUNet(
+        num_classes=args.num_classes, 
+        channels_shrink_factor=args.squeeze_unet_channels_shrink_factor
+    )
+    model = model.to(device)
+
+    print_num_trainable_parameters(model, args.epochs_color)
+    
+    return model
+
+
+### --- model --- ###
+
+################################################################################
+
+### --- data --- ###
+
+def _get_dataset(base_dir, list_dir, split, transform):
+    return Synapse_dataset(
+        base_dir=base_dir, list_dir=list_dir, split=split, transform=transform
+    )
+
+def _get_dataloader(dataset, batch_size, shuffle, num_workers, pin_memory) -> DataLoader:
+    return DataLoader(
+        dataset=dataset, batch_size=batch_size, shuffle=shuffle, 
+        num_workers=num_workers, pin_memory=pin_memory
+    )
+
+### --- data --- ###
+
+################################################################################
+
+### --- Weights and Biases --- ###
+
+def _wandb_init(args: args, model: torch.nn.Module):
+
+    wandb_config = args.get_args()
+    wandb_config.update(
+        {
+            "num_trainable_parameters": get_num_trainable_parameters(model)
+        }
+    )
+
+    wandb_run = wandb.init(project=args.project_name, config=wandb_config, mode=args.wandb_mode)
+
+    if args.wandb_watch_model:
+        wandb_run.watch(model, log='all')
+
+    return wandb_run
+
+
+### --- Weights and Biases --- ###
+
+################################################################################
+
+### --- training --- ###
+
+def _add_train_prog_bar_tasks(args: args, prog_bar: Progress, num_batches_train: int):
+
+    prog_bar_epochs_task = prog_bar.add_task(description=args.epochs_task_descr, total=args.num_epochs)
+    prog_bar_train_batches_task = prog_bar.add_task(description=args.train_batches_task_descr, total=num_batches_train)
+
+    return prog_bar_epochs_task, prog_bar_train_batches_task
+
+
+def _train(
+    args: args, prog_bar: Progress, device, model: torch.nn.Module, 
+    optimizer: torch.optim.SGD, dl_train: DataLoader, dl_val: DataLoader,
+    wb_run: Run
+):
+
+    max_iterations = args.num_epochs * len(dl_train)
+    iter_num = 0
+    
+    os.makedirs(args.checkpoint_dir) if not os.path.exists(args.checkpoint_dir) else None
+
+    num_batches_train = int(len(dl_train) * args.lim_num_batches_percent_train)
+    if num_batches_train == 0:
+        num_batches_train = 1
+
+    num_batches_val = int(len(dl_val) * args.lim_num_batches_percent_val)
+    if num_batches_val == 0:
+        num_batches_val = 1
+
+    prog_bar_epochs_task, prog_bar_train_batches_task = _add_train_prog_bar_tasks(args, prog_bar, num_batches_train)
+    prog_bar_val_batches_task, prog_bar_val_slices_task, prog_bar_val_metrics_task = _add_val_prog_bar_tasks(args, prog_bar, num_batches_val)
+
+    ce_loss = CrossEntropyLoss()
+    dice_loss = DiceLoss(args.num_classes)
+
+    best_epoch_loss_ce_train = torch.inf
+    best_epoch_loss_dice_train = torch.inf
+    best_epoch_loss_train = torch.inf
+    
+    best_epoch_metric_dice_val = 0
+    best_epoch_metric_jaccard_val = 0
+
+    ### --- epoch --- ###
+
+    for epoch in range(1, args.num_epochs + 1):
+        ### --- train step --- ###
+        
+        prog_bar.reset(prog_bar_train_batches_task)
+        prog_bar.reset(prog_bar_val_batches_task)
+        prog_bar.reset(prog_bar_val_slices_task)
+        prog_bar.reset(prog_bar_val_metrics_task)
+
+        running_loss_ce_train = 0
+        running_loss_dice_train = 0
+        running_loss_train = 0
+        train_ce_loss_is_best = False
+        train_dice_loss_is_best = False
+        train_loss_is_best = False
+        val_dice_is_best = False
+        val_jaccard_is_best = False
+
+        model.train()
+
+        for batch_train in list(dl_train)[:num_batches_train]:
+
+            img_batch_train  = batch_train["image"].to(device)
+            gt_batch_train   = batch_train["label"].to(device)
+            
+            img_batch_train = img_batch_train.unsqueeze(1)
+
+            outputs = model(img_batch_train)
+            # outputs.shape matches TransU-Net's output shape!!!
+
+            step_loss_ce_train = ce_loss.forward(outputs, gt_batch_train[:].long())
+            running_loss_ce_train += step_loss_ce_train
+            step_loss_dice_train = dice_loss.forward(outputs, gt_batch_train, softmax=True)
+            running_loss_dice_train += step_loss_dice_train
+            loss_train = args.alpha * step_loss_ce_train + args.beta * step_loss_dice_train
+            running_loss_train += loss_train
+
+            optimizer.zero_grad()
+            loss_train.backward()
+            optimizer.step()
+            
+            if args.use_lr_scheduler:
+                lr_ = args.base_lr * (1.0 - iter_num / max_iterations) ** 0.9
+                for param_group in optimizer.param_groups:
+                    param_group['lr'] = lr_
+
+            iter_num = iter_num + 1
+
+            prog_bar.advance(prog_bar_train_batches_task, 1)
+            prog_bar.advance(prog_bar_epochs_task, 1 / (num_batches_train))
+        
+        ### --- train step --- ###
+        
+        ########################################################################
+
+### --- training --- ###
+
+################################################################################
+
+### --- validation --- ###
+
+def _add_val_prog_bar_tasks(args: args, prog_bar: Progress, num_batches_val: int):
+    prog_bar_val_batches_task = prog_bar.add_task(description=args.val_batches_task_descr, total=num_batches_val)
+    prog_bar_val_slices_task = prog_bar.add_task(description=args.val_slices_task_descr, total=69)
+    prog_bar_val_metrics_task = prog_bar.add_task(description=args.val_metrics_task_descr, total=args.num_classes)
+
+    return prog_bar_val_batches_task, prog_bar_val_slices_task, prog_bar_val_metrics_task
+
+### --- validation --- ###
+
+################################################################################
+
+### --- main --- ###
+
+def main():
+
+    # args
+    print(f"Arguments:")
+    pprint(args.get_args())
+    print()
+
+    # device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print_device_name(device)
+
+    # model
+    model = _init_model(args, device)
+    
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=args.base_lr, momentum=args.momentum, weight_decay=args.weight_decay
+    )
+
+    # data
+    ds_train = _get_dataset(base_dir=args.train_root_path, list_dir=args.list_dir, split="train", transform=args.train_transforms)
+    dl_train = _get_dataloader(ds_train, args.batch_size, True, args.num_workers, pin_memory=args.pin_memory)
+    ds_val = _get_dataset(base_dir=args.val_volume_path, list_dir=args.list_dir, split="val_vol", transform=args.val_transforms)
+    dl_val = _get_dataloader(ds_val, 1, False, 1, True)
+    ds_test = _get_dataset(base_dir=args.test_volume_path, list_dir=args.list_dir, split="test_vol", transform=args.test_transforms)
+    dl_test = _get_dataloader(ds_test, 1, False, 1, True)
+    print_data_summary(args, ds_train, dl_train, ds_val, dl_val, ds_test, dl_test)
+
+    # Weights and Biases
+    wb_run = _wandb_init(args, model)
+
+    # progress
+    prog_bar = get_progress_bar()
+    prog_bar.start()
+
+    # training (and validation!)
+    _train(args, prog_bar, device, model, optimizer, dl_train, dl_val, wb_run)
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+### --- main --- ###
+
+################################################################################
---
+++ SqueezeU-Net/utilities/Prints.py
@@ -0,0 +1,95 @@
+from rich import print
+
+import torch
+
+from utilities.Model import get_num_trainable_parameters
+
+### --- device --- ###
+
+def print_device_name(device: torch.cuda.device):
+    
+    if device.type == "cuda":
+        device_name = torch.cuda.get_device_name(device)
+        
+        if "NVIDIA" in device_name:
+            device_name = f"[#008000][b]{device_name}[/b][/#008000]"
+        
+        print(f"Device: {device_name}\n")
+
+### --- device --- ###
+
+################################################################################
+
+### --- data --- ###
+
+def print_data_summary(
+    args, dataset_train, dataloader_train, dataset_val, dataloader_val, dataset_test, dataloader_test
+):
+    print(f"Total number of train slices      : {len(dataset_train)}")
+    print(f"Total number of train batches     : {len(dataloader_train)}")
+    print(f"Number of train batches limited to: {args.lim_num_batches_percent_train*100}%\n")
+    print(f"Total number of val cases (volumes): {len(dataset_val)}")
+    print(f"Total number of val batches        : {len(dataloader_val)}")
+    print(f"Number of val batches limited to   : {args.lim_num_batches_percent_val*100}%\n")
+    print(f"Total number of test cases (volumes): {len(dataset_test)}")
+    print(f"Total number of test batches        : {len(dataloader_test)}")
+    print(f"Number of test batches limited to   : {args.lim_num_batches_percent_test*100}%\n")
+
+
+### --- data --- ###
+
+################################################################################
+
+### --- model --- ###
+
+def print_num_trainable_parameters(model: torch.nn.Module, num_color: str = "#000000"):
+    
+    print(f"Number of trainable parameters: [{num_color}][b]{get_num_trainable_parameters(model)}[/b][/{num_color}]\n")
+
+### --- model --- ###
+
+################################################################################
+
+### --- training --- ###
+
+def print_end_of_epoch_summary(
+    args, epoch,
+    epoch_loss_ce_train, train_ce_is_best,
+    epoch_loss_dice_train, train_dice_is_best,
+    epoch_metric_jaccard_val, val_jaccard_is_best,
+    epoch_metric_dice_val, val_dice_is_best
+):
+    print(
+        f"[b][{args.epochs_color}]{epoch:03d}[/{args.epochs_color}][/b] | train | "
+        f"Cross-Entropy loss [b][{args.train_batches_color}]{epoch_loss_ce_train.item():02.6f}[/{args.train_batches_color}][/b] {args.loss_is_best_str if train_ce_is_best else args.loss_is_not_best_str} | "
+        f"Dice loss   [b][{args.train_batches_color}]{epoch_loss_dice_train.item():02.6f}[/{args.train_batches_color}][/b] {args.loss_is_best_str if train_dice_is_best else args.loss_is_not_best_str} |"
+        f"\n"
+        f"    | val   | "
+        f"Jaccard metric     [b][{args.val_batches_color}]{epoch_metric_jaccard_val:02.6f}[/{args.val_batches_color}][/b] {args.metric_is_best_str if val_jaccard_is_best else args.metric_is_not_best_str} | "
+        f"Dice metric [b][{args.val_batches_color}]{epoch_metric_dice_val:02.6f}[/{args.val_batches_color}][/b] {args.metric_is_best_str if val_dice_is_best else args.metric_is_not_best_str} |"
+    )
+
+    if epoch + 1 != args.num_epochs:
+        print()
+
+### --- training --- ###
+
+################################################################################
+
+### --- test --- ###
+
+def print_end_of_test_summary(
+        args, 
+        epoch_metric_jaccard_test, test_jaccard_is_best,
+        epoch_metric_dice_test, test_dice_is_best
+    ):
+
+    print(
+        f"    | test  | "
+        f"Jaccard metric     [b][{args.test_batches_color}]{epoch_metric_jaccard_test:02.6f}[/{args.test_batches_color}][/b] {args.metric_is_best_str if test_jaccard_is_best else args.metric_is_not_best_str} | "
+        f"Dice metric [b][{args.test_batches_color}]{epoch_metric_dice_test:02.6f}[/{args.test_batches_color}][/b] {args.metric_is_best_str if test_dice_is_best else args.metric_is_not_best_str} |"
+    )
+
+    print()
+
+################################################################################
